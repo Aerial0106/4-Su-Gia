b783ccc0d8d5af4ef8fd7d60dbb069f25a32dd90
Dinh Nguyen Nhat Quang <quangdinh0106@gmail.com>
2025-03-11 09:53:58 +0700

    ...

--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -2,14 +2,14 @@ var express = require("express");
 var router = express.Router();
 
 //user
-router.use("/home", require(__dirname + "/index"));
+router.use("/home", require(__dirname + "/homecontroller"));
 router.use("/shop", require(__dirname + "/shopcontroller"));
 router.use("/about", require(__dirname + "/aboutcontroller"));
 router.use("/contact", require(__dirname + "/contactcontroller"));
 router.use("/vegetables", require(__dirname + "/vegetablescontroller"));
 
 //admin
-router.use("/admin", require(__dirname + "/admin/admincontroller"));
+//router.use("/admin", require(__dirname + "/admin/admincontroller"));
 
 router.get("/", function (req, res) {
     res.render('index.ejs');
